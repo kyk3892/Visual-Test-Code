3203de8b4a211d64b33fc36f62078b538d8a565b
kyk3892 <kyk3892@naver.com>
2020-06-12 21:01:19 +0900

    a......

--- Coding Game with python/actuality4_1.py
+++ Coding Game with python/actuality4_1.py
@@ -3,19 +3,23 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
-pygame.init()
+pygame.init() #pygame 모듈을 초기화
 SURFACE = pygame.display.set_mode((400,300))
-pygame.display.set_caption("JUST WINDOWS")
+pygame.display.set_caption("JUST WINDOWS") #윈도의 타이틀
+
 
 def main():
     """main routine"""
     while True:
-        SURFACE.fill((255,255,255))
+        SURFACE.fill((255,255,0))
+
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
         pygame.display.update()
+
+        
 if __name__=='__main__':
     main()
